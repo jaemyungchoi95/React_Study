cc4b7fc3c1a90183ef6653c8a39755ae209a608f
ExProcurement <160109697+ExProcurement@users.noreply.github.com>
2024-11-26 01:22:50 +0900

    감정일기장 프로젝트 중반 update

---
+++ section12/src/components/DiaryItem.jsx
@@ -0,0 +1,32 @@
+import { getEmotionImage } from "../util/get-emotion-image";
+import "./DiaryItem.css";
+import Button from "./Button";
+import { useNavigate } from "react-router-dom";
+
+const DiaryItem = ({ id, emotionId, createdDate, content}) => {
+
+    const nav = useNavigate();
+
+    return (
+        <div className="DiaryItem">
+            <div onClick={() => nav(`/diary/${id}`)} className={`img_section img_section_${emotionId}`}>
+                <img src={getEmotionImage(emotionId)} alt="이미지 로딩중"/>
+            </div>
+
+            <div onClick={() => nav(`/diary/${id}`)} className="info_section">
+                <div className="created_date">
+                    {new Date(createdDate).toLocaleDateString()}
+                </div>
+                <div className="content">
+                    {content}
+                </div>
+            </div>
+
+            <div onClick={() => nav(`/edit/${id}`)} className="button_section">
+                <Button text={"수정하기"} />
+            </div>
+        </div>
+    );
+};
+
+export default DiaryItem;
